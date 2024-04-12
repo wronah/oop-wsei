f28edab62d8ecd08a2691a15d0e9adab46b37b15
wronah <hubertwro7@gmail.com>
2024-04-12 18:26:38 +0200

    add .gitignore, add class library with matrix2d class, create fields with constructor

---
+++ matrix/ClassLibrary/Matrix2D.cs
@@ -0,0 +1,18 @@
+﻿namespace ClassLibrary
+{
+    public class Matrix2D
+    {
+        private int a;
+        private int b;
+        private int c;
+        private int d;
+
+        public Matrix2D(int a, int b, int c, int d)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            this.d = d;
+        }
+    }
+}
